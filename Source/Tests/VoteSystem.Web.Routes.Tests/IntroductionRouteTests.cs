0881c3be3a073727f030b2bb0a294a33f43cd2a1
Andriyan Krastev <andrian_29@abv.bg>
2016-02-13 19:30:16 +0200

    Added unit tests.

---
+++ Source/Tests/VoteSystem.Web.Routes.Tests/IntroductionRouteTests.cs
@@ -0,0 +1,31 @@
+﻿namespace VoteSystem.Web.Routes.Tests
+{
+    using System.Web.Routing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MvcRouteTester;
+    using Controllers;
+
+    [TestClass]
+    public class IntroductionRouteTests
+    {
+        [TestMethod]
+        public void HasIntroRoute()
+        {
+            const string route = "/Introduction/Intro";
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            RouteAssert.HasRoute(routeCollection, route);
+            //routeCollection.ShouldMap(route).To<IntroductionController>( x => x.Intro());
+        }
+
+        //[TestMethod]
+        //public void IntroRouteWithIndex()
+        //{
+        //    const string route = "/Introduction/Intro";
+        //    var routeCollection = new RouteCollection();
+        //    RouteConfig.RegisterRoutes(routeCollection);
+        //    RouteAssert.HasRoute(routeCollection, route);
+        //    //routeCollection.ShouldMap(route).To<IntroductionController>( x => x.Intro());
+        //}
+    }
+}
--- Source/Web/VoteSystem.Web/App_Start/RouteConfig.cs
+++ Source/Web/VoteSystem.Web/App_Start/RouteConfig.cs
@@ -10,9 +10,14 @@
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Introduction", action = "Intro", id = UrlParameter.Optional });
+                name: "Intro",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Introduction", action = "Intro" });
+
+            //routes.MapRoute(
+            //   name: "Default",
+            //   url: "{controller}/{action}/{id}",
+            //   defaults: new { controller = "Introduction", action = "Intro", id = UrlParameter.Optional });
         }
     }
 }
