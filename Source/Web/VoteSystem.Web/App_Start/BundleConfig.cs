23ee36c88c346c1a89204a7cce9b65ef208f4fa4
Andriyan Krastev <andrian_29@abv.bg>
2016-02-14 08:48:48 +0200

    Added register form and social buttons.

--- Source/Web/VoteSystem.Web/App_Start/BundleConfig.cs
+++ Source/Web/VoteSystem.Web/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/bootstrap-social.css"));
+            bundles.Add(new StyleBundle("~/Content/forms-css").Include("~/Content/css/forms.css"));
         }
     }
 }
