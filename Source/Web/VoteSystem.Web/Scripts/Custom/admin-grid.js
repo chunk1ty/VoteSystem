4f85f1355eb1c007c24f5b61b134a5f599490b3a
Andriyan Krastev <andrian_29@abv.bg>
2016-02-27 00:19:12 +0200

    Added administration grid.

---
+++ Source/Web/VoteSystem.Web/Scripts/Custom/admin-grid.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        $('#admin-grid').dataTable({
+            pagingType: "full_numbers",
+            "oLanguage": {
+                "sSearch": "Search by System name: "
+            },
+            columnDefs: [{
+                targets: [0],
+            }, {
+                targets: [1],
+                "sortable": false
+            }, {
+                targets: [2],
+                "searchable": false,
+                "sortable": false
+            }, {
+                targets: [3],
+                "searchable": false,
+                "sortable": false
+            }]
+        });
+    });
+}());
