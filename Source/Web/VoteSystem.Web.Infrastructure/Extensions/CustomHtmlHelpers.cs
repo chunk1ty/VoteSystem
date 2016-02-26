5e21dc17e64160845b602b96c71e7c52531dc528
Andriyan Krastev <andrian_29@abv.bg>
2016-02-26 03:15:03 +0200

    Added jquery DataTable grid to home/index.

---
+++ Source/Web/VoteSystem.Web.Infrastructure/Extensions/CustomHtmlHelpers.cs
@@ -0,0 +1,35 @@
+﻿namespace VoteSystem.Web.Infrastructure.Extensions
+{
+    using System;
+    using System.Web.Mvc;
+
+    public static class CustomHtmlHelpers
+    {
+        public static string TimeDisplay(this HtmlHelper helper, DateTime endDate)
+        {
+            TimeSpan ts = endDate - DateTime.Now;
+
+            string timeAsString = string.Empty;
+            if (ts.Days <= 0)
+            {
+                timeAsString = String.Format("{0} hour{1} {2} minute{3}",
+                                                  ts.Hours,
+                                                  ts.Hours == 1 ? "" : "s",
+                                                  ts.Minutes,
+                                                  ts.Minutes == 1 ? "" : "s",
+                                                  ts.Days);
+            }
+            else
+            {
+                timeAsString = String.Format("{4} days {0} hour{1} {2} minute{3}",
+                                                      ts.Hours,
+                                                      ts.Hours == 1 ? "" : "s",
+                                                      ts.Minutes,
+                                                      ts.Minutes == 1 ? "" : "s",
+                                                      ts.Days);
+            }
+
+            return timeAsString;
+        }
+    }
+}
---
+++ Source/Web/VoteSystem.Web/Scripts/Custom/home-grid.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    $(document).ready(function () {
+        $('#example').dataTable({
+            pagingType: "full_numbers",
+            search: "Search all columns:",
+            "oLanguage": {
+                "sSearch": "Search by Name: "
+            },
+            columnDefs: [{
+                targets: [0],
+                "searchable": false,
+                "sortable": false
+            }, {
+                targets: [1]
+            }, {
+                targets: [2],
+                "searchable": false,
+                "sortable": false
+            }, {
+                targets: [3],
+                "searchable": false
+            }]
+        });
+    });
+}());
