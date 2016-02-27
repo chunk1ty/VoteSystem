b81be2c2e2f7fd8f91276d24ded8ccea5133da95
Andriyan Krastev <andrian_29@abv.bg>
2016-02-27 02:48:24 +0200

    Added calendars to create rate system page.

--- Source/Web/VoteSystem.Web/Scripts/Custom/home-grid.js
+++ Source/Web/VoteSystem.Web/Scripts/Custom/home-grid.js
@@ -1,27 +1,25 @@
-﻿(function () {
+﻿$(function ($) {
     "use strict";
 
-    $(document).ready(function () {
-        $('#example').dataTable({
-            pagingType: "full_numbers",
-            search: "Search all columns:",
-            "oLanguage": {
-                "sSearch": "Search by Name: "
-            },
-            columnDefs: [{
-                targets: [0],
-                "searchable": false,
-                "sortable": false
-            }, {
-                targets: [1]
-            }, {
-                targets: [2],
-                "searchable": false,
-                "sortable": false
-            }, {
-                targets: [3],
-                "searchable": false
-            }]
-        });
+    $('#example').dataTable({
+        pagingType: "full_numbers",
+        search: "Search all columns:",
+        "oLanguage": {
+            "sSearch": "Search by Name: "
+        },
+        columnDefs: [{
+            targets: [0],
+            "searchable": false,
+            "sortable": false
+        }, {
+            targets: [1]
+        }, {
+            targets: [2],
+            "searchable": false,
+            "sortable": false
+        }, {
+            targets: [3],
+            "searchable": false
+        }]
     });
-}());
+}($));
