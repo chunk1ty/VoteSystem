3b1904b90582eed737733aa1550fc4dd1b9dbb18
Andriyan Krastev <andrian_29@abv.bg>
2016-03-16 15:37:30 +0200

    Added logic for confirm new account.

--- Source/Web/VoteSystem.Web/App_Start/EmailService.cs
+++ Source/Web/VoteSystem.Web/App_Start/EmailService.cs
@@ -3,13 +3,46 @@
     using System.Threading.Tasks;
 
     using Microsoft.AspNet.Identity;
+    using SendGrid;
+    using System;
 
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+        public async Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            await configSendGridasync(message);
+        }
+
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridasync(IdentityMessage message)
+        {
+            var myMessage = new SendGridMessage();
+
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new System.Net.Mail.MailAddress("andriyan.krastev@gmail.com", "AK");
+            myMessage.Subject = message.Subject;
+            myMessage.Html = message.Body;
+
+            var transportWeb = new Web("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");
+            try
+            {
+                // Send the email.
+                if (transportWeb != null)
+                {
+                    await transportWeb.DeliverAsync(myMessage);
+                }
+                else
+                {
+                    //Trace.TraceError("Failed to create Web transport.");
+                    await Task.FromResult(0);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
         }
     }
 }
