ae359ac116a088f7b35076b7c09100d638a248c7
Andriyan Krastev <andrian_29@abv.bg>
2016-03-17 02:37:17 +0200

    Added mime type in web config.

---
+++ Source/Web/VoteSystem.Web.Infrastructure/Extensions/PrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace VoteSystem.Web.Infrastructure.Extensions
+{
+    using System.Security.Principal;
+
+    using VoteSystem.Common;
+
+    public static class PrincipalExtensions
+    {
+        public static bool IsAdministrator(this IPrincipal principal)
+        {
+            if (principal == null)
+            {
+                return false;
+            }
+
+            return principal.IsInRole(GlobalConstants.AdministratorRoleName);
+        }
+    }
+}
