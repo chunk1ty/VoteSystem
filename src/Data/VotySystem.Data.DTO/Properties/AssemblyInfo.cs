194e6e7d654f3cfc2b18c26e6b22f3586ca8cca5
Andriyan Krastev <andriyan.krastev@gmail.com>
2017-04-05 01:10:59 +0300

    i need to sleep ...

--- src/Data/VotySystem.Data.DTO/Properties/AssemblyInfo.cs
+++ src/Data/VotySystem.Data.DTO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VoteSystem.Data.Contracts")]
+[assembly: AssemblyTitle("VotySystem.Data.DTO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VoteSystem.Data.Contracts")]
+[assembly: AssemblyProduct("VotySystem.Data.DTO")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("70a837c7-0e10-4709-9aa6-0dfec60ec87b")]
+[assembly: Guid("e73cb871-87eb-4f75-81be-121a95f50736")]
 
 // Version information for an assembly consists of the following four values:
 //
