9fe9b9d270906737f5ae30085b5bc5e7bddbe31f
Andriyan Krastev <andrian_29@abv.bg>
2017-04-29 15:58:18 +0300

    Update AspNetUser.cs

--- src/Data/VoteSystem.Data.Ef/Models/AspNetUser.cs
+++ src/Data/VoteSystem.Data.Ef/Models/AspNetUser.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Security.Claims;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
